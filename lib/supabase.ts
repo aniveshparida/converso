28109f29932b8a6ac51c49f826542fdae1774860
aniveshparida <aniveshparida8@gmail.com>
2025-12-17 19:42:45 +0530

    changed superbase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,9 @@
-import {createClient} from "@supabase/supabase-js";
-import {auth} from "@clerk/nextjs/server";
+// lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
 export const createSupabaseClient = () => {
-    return createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!, {
-            async accessToken() {
-                return ((await auth()).getToken());
-            }
-        }
-    )
-}
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
+  );
+};
